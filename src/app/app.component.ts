a1a6c3c3a9fd576210a350a0194747cb029aace5
ImsirovicAjdin <ajdika@live.com>
2019-10-30 11:58:29 +0100

    Remove redundant files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  templateUrl: './app.component.html'
 })
-export class AppComponent  {
-  name = 'Angular';
-}
+export class AppComponent  {}
